459ef9b997a593a41eaff1932988085844cc9bb7
srt19 <90868790+srt19@users.noreply.github.com>
2022-02-14 00:20:13 +0700

    Fixed freezing GUI while loading model

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 import sys
-from PyQt6.QtCore import Qt, QRect, QSize, QMetaObject, QCoreApplication
+from PyQt6.QtCore import (
+	Qt, QRect, QSize, QMetaObject, QCoreApplication, QObject, QThread, pyqtSignal
+)
 from PyQt6.QtGui import QFont
 from PyQt6.QtWidgets import (
 	QWidget, QPushButton, QComboBox, QPlainTextEdit, QFrame, QMenuBar, QStatusBar, QApplication, QMainWindow
 )
+
+class Worker(QObject):
+	finished = pyqtSignal()
+	progress = pyqtSignal(int)
+	
+	#Loading Model
+	def load_job(self):
+		from transformers import MarianTokenizer, AutoModelForSeq2SeqLM
+
+		global mname
+		global model
+		global tokenizer
+		tokenizer = MarianTokenizer.from_pretrained(mname, local_files_only=True)
+		model = AutoModelForSeq2SeqLM.from_pretrained(mname, local_files_only=True)
+		print("Loading Finished")
+		self.finished.emit()
 
 class Ui_MainWindow(object):
 	def setupUi(self, MainWindow):
@@ -37,14 +55,14 @@ class Ui_MainWindow(object):
 		self.lang_list.setSizeAdjustPolicy(QComboBox.SizeAdjustPolicy.AdjustToContents)
 		self.lang_list.setObjectName("lang_list")
 		self.output = QPlainTextEdit(self.centralwidget)
-		self.output.setGeometry(QRect(410, 150, 380, 350))
+		self.output.setGeometry(QRect(410, 150, 380, 400))
 		font.setFamily("Arial")
 		font.setPointSize(11)
 		self.output.setFont(font)
 		self.output.setFrameShadow(QFrame.Shadow.Plain)
 		self.output.setObjectName("output")
 		self.input = QPlainTextEdit(self.centralwidget)
-		self.input.setGeometry(QRect(10, 150, 380, 350))
+		self.input.setGeometry(QRect(10, 150, 380, 400))
 		font.setFamily("Arial")
 		font.setPointSize(11)
 		self.input.setFont(font)
@@ -72,29 +90,54 @@ class Ui_MainWindow(object):
 	#Get Language Number
 	def lang_select(self, i):
 		lang = i
-
-	#Loading Model
+	
+	#Loading Model Thread
 	def load_run(self):
-		from transformers import MarianTokenizer, AutoModelForSeq2SeqLM
-		
-		global mname
-		global model
-		global tokenizer
-		
+		global mname		
 		if lang == 0:
 			print("Japanese to English Selected")
 			mname = "models/Helsinki-NLP/opus-mt-ja-en"
 		elif lang == 1:
 			print("English to Japanese Selected")
 			mname = "models/Helsinki-NLP/opus-mt-en-ja"
+		elif lang == 2:
+			print("Chinese to English Selected")
+			mname = "models/Helsinki-NLP/opus-mt-zh-en"
+		elif lang == 3:
+			print("English to Chinese Selected")
+			mname = "models/Helsinki-NLP/opus-mt-en-zh"
+		elif lang == 4:
+			print("Indonesia to English Selected")
+			mname = "models/Helsinki-NLP/opus-mt-id-en"
+		elif lang == 5:
+			print("English to Indonesia Selected")
+			mname = "models/Helsinki-NLP/opus-mt-en-id"
+		elif lang == 6:
+			print("Vietnam to English Selected")
+			mname = "models/Helsinki-NLP/opus-mt-vi-en"
+		elif lang == 7:
+			print("English to Vietnam Selected")
+			mname = "models/Helsinki-NLP/opus-mt-en-vi"
 		print("Loading Model Files")
-		tokenizer = MarianTokenizer.from_pretrained(mname, local_files_only=True)
-		model = AutoModelForSeq2SeqLM.from_pretrained(mname, local_files_only=True)
-		print("Loading Finished")		
-
-	#Translating Start
+		
+		self.thread = QThread()
+		self.worker = Worker()
+		self.worker.moveToThread(self.thread)
+		
+		self.thread.started.connect(self.worker.load_job)
+		self.worker.finished.connect(self.thread.quit)
+		self.worker.finished.connect(self.worker.deleteLater)
+		self.thread.finished.connect(self.thread.deleteLater)
+		
+		self.thread.start()
+		
+		self.LoadModel.setEnabled(False)
+		self.thread.finished.connect(
+			lambda: self.LoadModel.setEnabled(True)
+		)
+	
+	#Translating
 	def tl_run(self):
-		global tl_text
 		tl_text = self.input.toPlainText()
 		print("Translating")
 		input_ids = tokenizer.encode(tl_text, return_tensors="pt")
@@ -110,14 +153,19 @@ class Ui_MainWindow(object):
 		self.Translate.setText(_translate("MainWindow", "Translate"))
 		self.output.setToolTip(_translate("MainWindow", "Translated Text Goes Here"))
 		self.output.setPlaceholderText(_translate("MainWindow", "Output Text Here"))
-		self.input.setToolTip(_translate("MainWindow", "Translated Text Goes Here"))
+		self.input.setToolTip(_translate("MainWindow", "Your Text Goes Here"))
 		self.input.setPlaceholderText(_translate("MainWindow", "Input Your Text Here"))
 		#Language List
 		self.lang_list.addItem("Japanese to English")
 		self.lang_list.addItem("English to Japanese")
+		self.lang_list.addItem("Chinese to English")
+		self.lang_list.addItem("English to Chinese")
+		self.lang_list.addItem("Indonesia to English")
+		self.lang_list.addItem("English to Indonesia")
+		self.lang_list.addItem("Vietnam to English")
+		self.lang_list.addItem("English to Vietnam")
 
 if __name__ == "__main__":
-	import sys
 	app = QApplication(sys.argv)
 	MainWindow = QMainWindow()
 	ui = Ui_MainWindow()
