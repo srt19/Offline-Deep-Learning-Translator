e4f4b8e88b8ee437684f920e71d013884dd8f5ea
srt19 <90868790+srt19@users.noreply.github.com>
2022-03-03 00:40:04 +0700

    Updated download tab placeholder

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ class Worker(QObject):
 		if lang_id == 8:
 			inputs = tokenizer(tl_input, return_tensors="pt")
 			translated_tokens = model.generate(**inputs, decoder_start_token_id=tokenizer.lang_code_to_id["en_XX"],
-											   early_stopping=True, max_length=48)
+												early_stopping=True, max_length=48)
 			tl_output = tokenizer.batch_decode(translated_tokens, skip_special_tokens=True)[0]
 		else:
 			input_ids = tokenizer.encode(tl_input, return_tensors="pt")
@@ -79,6 +79,7 @@ class MainWindow(QMainWindow):
 		self.unload_button = QPushButton("Unload Model")
 		self.unload_button.clicked.connect(self.unload_run)
 		self.unload_button.setStatusTip("Click to unload model files")
+		self.unload_button.setEnabled(False)
 		self.in_text = QPlainTextEdit()
 		self.in_text.setStatusTip("Type your text here")
 		self.in_text.setMinimumSize(QSize(380, 430))
@@ -89,9 +90,7 @@ class MainWindow(QMainWindow):
 		self.lang_list = QComboBox()
 		self.lang_list.setStatusTip("Select the language model")
 		global lang_id
-		global mname
 		lang_id = 0
-		mname = 0
 		self.lang_list.addItem("Japanese to English")
 		self.lang_list.addItem("English to Japanese")
 		self.lang_list.addItem("Chinese to English")
@@ -117,6 +116,31 @@ class MainWindow(QMainWindow):
 		self.main_tab_layout.addWidget(self.in_text, 5, 0, 1, 1)
 		self.main_tab_layout.addWidget(self.out_text, 5, 1, 1, 1)
 		self.main_tab.setLayout(self.main_tab_layout)
+		
+		# DL Tab
+		global dl_id
+		dl_id = 0
+		self.dl_label = QLabel()
+		self.dl_label.setText("Hello")
+		self.dl_button = QPushButton("Download")
+		self.dl_button.clicked.connect(self.dl_run)
+		self.dl_combo = QComboBox()
+		self.dl_combo.addItem("Japanese to English")
+		self.dl_combo.addItem("English to Japanese")
+		self.dl_combo.addItem("Chinese to English")
+		self.dl_combo.addItem("English to Chinese")
+		self.dl_combo.addItem("Indonesia to English")
+		self.dl_combo.addItem("English to Indonesia")
+		self.dl_combo.addItem("Vietnam to English")
+		self.dl_combo.addItem("English to Vietnam")
+		self.dl_combo.addItem("Japanese to English MBart")
+		self.dl_combo.currentIndexChanged.connect(self.dl_num)
+		
+		self.dl_layout = QGridLayout()
+		self.dl_layout.addWidget(self.dl_combo, 0, 0, 1, 1)
+		self.dl_layout.addWidget(self.dl_button, 1, 0, 1, 1)
+		self.dl_layout.addWidget(self.dl_label, 2, 0, 1, 1)
+		self.dl_tab.setLayout(self.dl_layout)
 
 		layout.addWidget(self.tabs)
 		self.tab_widget.setLayout(layout)
@@ -130,46 +154,55 @@ class MainWindow(QMainWindow):
 		self.statusbar.addPermanentWidget(self.statuslabel)
 		self.statusbar.addPermanentWidget(self.modellabel)
 
-	@staticmethod
-	def lang_sel(i):
+	def lang_sel(self, i):
 		global lang_id
 		lang_id = i
 
 	def load_run(self):
 		global mname
 		if lang_id == 0:
-			mname = "./models/Helsinki-NLP/opus-mt-ja-en"
+			mname = "models/Helsinki-NLP/opus-mt-ja-en"
 			self.modellabel.setText("Japanese to English")
+
 		elif lang_id == 1:
-			mname = "./models/Helsinki-NLP/opus-mt-en-ja"
+			mname = "models/Helsinki-NLP/opus-mt-en-ja"
 			self.modellabel.setText("English to Japanese")
+
 		elif lang_id == 2:
-			mname = "./models/Helsinki-NLP/opus-mt-zh-en"
+			mname = "models/Helsinki-NLP/opus-mt-zh-en"
 			self.modellabel.setText("Chinese to English")
+
 		elif lang_id == 3:
-			mname = "./models/Helsinki-NLP/opus-mt-en-zh"
+			mname = "models/Helsinki-NLP/opus-mt-en-zh"
 			self.modellabel.setText("English to Chinese")
+
 		elif lang_id == 4:
-			mname = "./models/Helsinki-NLP/opus-mt-id-en"
+			mname = "models/Helsinki-NLP/opus-mt-id-en"
 			self.modellabel.setText("Indonesia to English")
+
 		elif lang_id == 5:
-			mname = "./models/Helsinki-NLP/opus-mt-en-id"
+			mname = "models/Helsinki-NLP/opus-mt-en-id"
 			self.modellabel.setText("English to Indonesia")
+
 		elif lang_id == 6:
-			mname = "./models/Helsinki-NLP/opus-mt-vi-en"
+			mname = "models/Helsinki-NLP/opus-mt-vi-en"
 			self.modellabel.setText("Vietnam to English")
+
 		elif lang_id == 7:
-			mname = "./models/Helsinki-NLP/opus-mt-en-vi"
+			mname = "models/Helsinki-NLP/opus-mt-en-vi"
 			self.modellabel.setText("English to Vietnam")
+
 		elif lang_id == 8:
-			mname = "./models/ken11/mbart-ja-en"
+			mname = "models/ken11/mbart-ja-en"
 			self.modellabel.setText("Japanese to English MBart")
+
 		mfound = os.path.exists(mname)
+
 		if not mfound:
 			self.modellabel.setText("Model Files Not Found")
 			self.no_file()
-			mname = 0
 			return
+
 		self.thread = QThread()
 		self.worker = Worker()
 		self.worker.moveToThread(self.thread)
@@ -183,16 +216,15 @@ class MainWindow(QMainWindow):
 		self.load_button.setEnabled(False)
 		self.unload_button.setEnabled(False)
 		self.statuslabel.setText("Loading Model Files")
-		self.thread.finished.connect(lambda: self.statuslabel.setText("Ready"))
-		self.thread.finished.connect(lambda: self.load_button.setEnabled(True))
 		self.thread.finished.connect(lambda: self.unload_button.setEnabled(True))
 		self.thread.finished.connect(lambda: self.tl_button.setEnabled(True))
+		self.thread.finished.connect(lambda: self.statuslabel.setText("Ready"))
 
 	def no_file(self):
 		button = QMessageBox.warning(
 			self,
 			"Model Files Not Found",
-			"Please make sure you have downloaded the model files and placed in the right models folder.",
+			"Please make sure you have downloaded the model files and placed it in the right folder.",
 			buttons=QMessageBox.StandardButton.Ok,
 		)
 
@@ -204,8 +236,10 @@ class MainWindow(QMainWindow):
 		tokenizer = " "
 		model = " "
 		gc.collect(2)
-		self.modellabel.setText("No model loaded")
 		self.tl_button.setEnabled(False)
+		self.unload_button.setEnabled(False)
+		self.load_button.setEnabled(True)
+		self.modellabel.setText("No model loaded")
 
 	def tl_run(self):
 		global tl_input
@@ -221,10 +255,22 @@ class MainWindow(QMainWindow):
 		self.thread.start()
 
 		self.tl_button.setEnabled(False)
+		self.load_button.setEnabled(False)
+		self.unload_button.setEnabled(False)
 		self.statuslabel.setText("Translating")
-		self.thread.finished.connect(lambda: self.statuslabel.setText("Ready"))
 		self.thread.finished.connect(lambda: self.tl_button.setEnabled(True))
+		self.thread.finished.connect(lambda: self.load_button.setEnabled(True))
+		self.thread.finished.connect(lambda: self.unload_button.setEnabled(True))
+		self.thread.finished.connect(lambda: self.statuslabel.setText("Ready"))
 		self.thread.finished.connect(lambda: self.out_text.setPlainText(tl_output))
+		
+	def dl_num(self, i):
+		global dl_id
+		dl_id = i
+	
+	def dl_run(self):
+		global dl_id
+		
 
 
 if __name__ == '__main__':
